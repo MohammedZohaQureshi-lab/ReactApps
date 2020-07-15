7861e9f4bfd4a337c09a94a3b141200cec2235ef
Zoha Qureshi <zoha.a.qureshi@avanade.com>
2020-07-15 22:03:41 +0530

    practie

--- react-readiness/src/components/Persons/Person/Person.jsx
+++ react-readiness/src/components/Persons/Person/Person.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary'
 
 class Persons extends Component {
-   render(){
-    console.log('[Person.js] render Method');
-    return (
-        <div className={classes.Person}>
-            <p onClick={this.props.clicked} >
-                My Names is {this.props.name}, I'm {this.props.age} years of age.
+    render() {
+        console.log('[Person.js] render Method');
+        return (
+            <Aux>
+                <div className={classes.Person}>
+                    <p onClick={this.props.clicked} >
+                        My Names is {this.props.name}, I'm {this.props.age} years of age.
             </p>
-            <div>
-                <label>Edit Name</label>
-                <input type="text" onChange={this.props.changeHandler} value={this.props.name} />
-            </div>
-            <div>{this.props.children}</div>
-        </div>
-    );
-   }
+                    <div>
+                        <label>Edit Name</label>
+                        <input type="text" onChange={this.props.changeHandler} value={this.props.name} />
+                    </div>
+                    <div>{this.props.children}</div>
+                </div>
+            </Aux>
+        );
+    }
 };
 
 export default Persons;
---
+++ react-readiness/src/hoc/Auxiliary.jsx
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+const aux = (props) => props.children;
+ 
+export default aux;
